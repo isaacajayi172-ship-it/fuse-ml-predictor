19ca1c4ca3472fae544bdd840a60bd64bb62be47
Isaac Ajayi <isaacajayi172@gmail.com>
2026-08-20 23:34:21 +0100

    Gnerated synthetic fuse data by making use of the I2C formula

--- generate_fuse_data.py
+++ generate_fuse_data.py
@@ -1,5 +1,16 @@
-k = 100
-current = 10
+import random
+import pandas as pd 
 
-time_taken_to_blow = k / (current ** 2)
-print(time_taken_to_blow)
+rows = []
+
+for i in range(1000):
+    k = random.uniform(50, 500)
+    current = random.uniform(1,50)
+    time_taken_to_blow = k / (current ** 2)
+    print(time_taken_to_blow)
+    rows.append({"k": k, "current": current, "time_taken_to_blow": time_taken_to_blow})
+
+data = pd.DataFrame(rows)
+print(data.head())
+data.to_csv("fuse_data.csv", index = False)
+
